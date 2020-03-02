6a7d80585fb6f2c10f36357ebfdf4b7cf226aa3b
Eric Zhao <wzhao.eric@gmail.com>
2020-03-02 17:35:05 -0500

    create a CustomMap class

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,15 @@
-import { User } from './User';
-import { Company } from './Company';
-const user = new User();
-const compnany = new Company();
+// import { User } from './User';
+// import { Company } from './Company';
+// const user = new User();
+// const compnany = new Company();
 
-console.log(user);
-console.log(compnany);
+// console.log(user);
+// console.log(compnany);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
